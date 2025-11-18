5beef1fe7dcce262f6c16a9b9e1fdd3d0b9243e4
Juaan00 <juquirogam@unal.edu.co>
2025-11-18 17:30:03 -0500

    agregar linter al Back y Front, añadir detalles al registro y login en Back

--- Proyecto/Backend/src/routes/authRoutes.ts
+++ Proyecto/Backend/src/routes/authRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { register, login } from "../controllers/authController";
+import { register, login, changePassword } from "../controllers/authController";
 
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/changepassword", changePassword);
 
 export default router;
